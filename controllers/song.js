fc496e394db771c6cf6b46107fee7f9ceee51951
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-06-18 12:09:45 -0600

    Delete a song

--- controllers/song.js
+++ controllers/song.js
@@ -10,46 +10,46 @@ var Song = require('../models/song');
 function getSong(req, res) {
     var songId = req.params.id;
     Song.findById(songId)
-    .populate({path:'album'}).exec()
-    .then((songStore) => {
-        if (!songStore) {
-            return res.status(404).send({ message: 'No existe el song' });
-        } else {
-            return res.status(200).send({ song: songStore });
-        }
-    })
-    .catch((err) => {
-        console.log(err);
-        return res.status(500).send({ message: 'Error al obtenr el song' });
-    });
+        .populate({ path: 'album' }).exec()
+        .then((songStore) => {
+            if (!songStore) {
+                return res.status(404).send({ message: 'No existe el song' });
+            } else {
+                return res.status(200).send({ song: songStore });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).send({ message: 'Error al obtenr el song' });
+        });
 }
 
-function getSongs(req, res){
+function getSongs(req, res) {
     var albumId = req.params.album;
-    if(!albumId){
+    if (!albumId) {
         var find = Song.find({}).sort('number');
-    }else{
-        var find = Song.find({album: albumId}).sort('number');
+    } else {
+        var find = Song.find({ album: albumId }).sort('number');
     }
 
     find.populate({
         path: 'album',
-        populate:{
-            path:'artist',
-            model:'Artist'
+        populate: {
+            path: 'artist',
+            model: 'Artist'
         }
     }).exec()
-    .then((songsStore) => {
-        if (!songsStore) {
-            return res.status(404).send({ message: 'No existe el songs' });
-        } else {
-            return res.status(200).send({ songs: songsStore });
-        }
-    })
-    .catch((err) => {
-        console.log(err);
-        return res.status(500).send({ message: 'Error al obtenr el songs' });
-    });
+        .then((songsStore) => {
+            if (!songsStore) {
+                return res.status(404).send({ message: 'No existe el songs' });
+            } else {
+                return res.status(200).send({ songs: songsStore });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).send({ message: 'Error al obtenr el songs' });
+        });
 }
 
 function saveSong(req, res) {
@@ -62,19 +62,59 @@ function saveSong(req, res) {
     song.album = params.album;
 
     song.save()
-        .then((songStored)=>{
-            if(!songStored){
-                return res.status(404).send({ message: 'No se ha guardado la cancion ' });    
+        .then((songStored) => {
+            if (!songStored) {
+                return res.status(404).send({ message: 'No se ha guardado la cancion ' });
             }
-            return res.status(200).send({song:songStored});
+            return res.status(200).send({ song: songStored });
         })
         .catch((err) => {
             console.log(err);
             return res.status(500).send({ message: 'Error en save song' });
         });
 }
+
+function updateSong(req, res) {
+    var songId = req.params.id;
+    var update = req.body;
+
+    Song.findByIdAndUpdate(songId, update)
+        .then((songUpdated) => {
+            if (!songUpdated) {
+                res.status(404).send({ message: 'No se ha actualizado song' });
+            } else {
+                res.status(200).send({ song: songUpdated });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).send({ message: 'Error en peticion update song' });
+        });
+}
+
+function deleteSong(req, res) {
+    var songId = req.params.id;
+    Song.findByIdAndDelete(songId)
+        .then(songRemoved => {
+            if (!songRemoved) {
+                return res.status(404).send({ message: 'El song no ha podido ser eliminado o no existe.' });
+            }
+
+            return res.status(200).send({
+                song: songRemoved,
+                message: 'song eliminados correctamente.'
+            })
+            .catch(err => {
+                console.error('Error al borrar el song ', err);
+                return res.status(500).send({ message: 'Error al borrar el song' });
+            });
+        });
+}
+
 module.exports = {
     getSong,
     saveSong,
     getSongs,
+    updateSong,
+    deleteSong
 }
--- routes/song.js
+++ routes/song.js
@@ -16,4 +16,6 @@ var md_upload = multipart({uploadDir: './uploads/songs'});
 api.get('/song/:id', md_auth.ensureAuth, SongController.getSong);
 api.post('/song', md_auth.ensureAuth, SongController.saveSong);
 api.get('/songs/:album?', md_auth.ensureAuth, SongController.getSongs);
+api.put('/song/:id', md_auth.ensureAuth, SongController.updateSong);
+api.delete('/song/:id', md_auth.ensureAuth, SongController.deleteSong);
 module.exports = api;
