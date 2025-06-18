9067de3ecce6c5a312502fb07a654207522bb274
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-06-18 12:30:12 -0600

    Upload files song

--- controllers/song.js
+++ controllers/song.js
@@ -111,10 +111,58 @@ function deleteSong(req, res) {
         });
 }
 
+function uploadfile(req, res) {
+    var songId = req.params.id;
+    var file_name = 'No subido...';
+
+    if (req.files) {
+        var file_path = req.files.file.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+        console.log(file_ext);
+        if (file_ext == 'mp3' ) {
+            Song.findByIdAndUpdate(songId, { file: file_name })
+                .then((songStored) => {
+                    if (!songStored) {
+                        return res.status(404).send({ message: 'no se actualizo el song porque no existe' });
+                    } else {
+                        return res.status(200).send({ song:songStored });
+                    }
+                })
+                .catch((err) => {
+                    console.log(err); // Log the error for debugging
+                    return res.status(500).send({ message: 'Error en la peticion de file' });
+                });
+        } else {
+            return res.status(200).send({ message: 'extencion del archivo no valida' });
+        }
+        console.log(file_path);
+    } else {
+        return res.status(200).send({ message: 'No ha subido ninguna imagen' });
+    }
+}
+
+function getsongFile(req, res){
+    var song_file = req.params.songFile;
+    var path_file = './uploads/songs/'+song_file;
+    console.log('Rose the file music  is '+path_file);
+    fs.exists(path_file, function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            return res.status(200).send({message: 'No existe la song'});
+        }
+    })
+}
 module.exports = {
     getSong,
     saveSong,
     getSongs,
     updateSong,
-    deleteSong
+    deleteSong,
+    getsongFile,
+    uploadfile
 }
